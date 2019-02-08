9f9e7952523eb1687ff4f05bf4cf62a82b380441
nicolas.bettiol <nicolas.bettiol@santexgroup.com>
2019-02-08 10:21:27 -0300

    refactor

--- app/server.ts
+++ app/server.ts
@@ -7,16 +7,17 @@ import { readFileSync } from 'fs';
 
 const  PORT = 9000;
 const http = require('http');
-var httpServer = http.createServer(app);
 
 const resolvers = require('./src/resolvers/resolvers');
 const typeDefs =  gql`${readFileSync('app/src/graphql/schema.graphql', 'utf8')}`;
 
 const apollo = new ApolloServer({typeDefs, resolvers});
 apollo.applyMiddleware({ app });
+
+var httpServer = http.createServer(app);
 apollo.installSubscriptionHandlers(httpServer)
 
 httpServer.listen({ port: PORT }, () => {
-  console.log(`🚀 Server ready at http://localhost:9000${apollo.graphqlPath}`)
-  console.log(`Subscriptions ready at ws://localhost:${PORT}${apollo.subscriptionsPath}`)
+  console.log(`Server ready at localhost:9000${apollo.graphqlPath}`)
+  console.log(`Subscriptions ready at localhost:${PORT}${apollo.subscriptionsPath}`)
 })
