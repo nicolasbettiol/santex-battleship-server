7a8b2361bccc9922cc957ecd1f14c89d7164ff27
nicolas.bettiol <nicolas.bettiol@santexgroup.com>
2019-02-11 16:04:28 -0300

    add games

--- app/src/resolvers/resolvers.ts
+++ app/src/resolvers/resolvers.ts
@@ -10,8 +10,9 @@ const Query = {
   game: (root, {id}) => db.games.get(id),
   player: (root, {id}) => db.players.get(id),
   boards: () => db.boards.list(),
-  players: () => db.players.list()
-};
+  players: () => db.players.list(),
+  games: () => db.games.list()
+}
 
 const Subscription = {
   newGameAdded: {
